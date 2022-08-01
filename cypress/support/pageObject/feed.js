4e3a51caee27cf6129aeb22e6c611cc9ef95bb54
Alex <take2smile@gmail.com>
2022-08-01 12:10:14 +0300

    feed author

--- cypress/support/pageObject/feed.js
+++ cypress/support/pageObject/feed.js
@@ -4,7 +4,8 @@ class feed {
 
         sidebarTag: (tagName) => cy.get('.sidebar > .tag-list').contains(`${tagName}`),
         yourFeedToggle: () => cy.get('.feed-toggle').contains('Your Feed'),
-        globalFeedToggle: () => cy.get('.feed-toggle').contains('Global Feed')
+        globalFeedToggle: () => cy.get('.feed-toggle').contains('Global Feed'),
+        globalFeedAuthor: () => cy.get('.article-preview').eq('0').find('.author').invoke('text')
     }
 
 }
