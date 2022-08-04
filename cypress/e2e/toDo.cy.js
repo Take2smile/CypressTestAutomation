48bdf38584deaa2f482fec33480e069248489a60
Alex <take2smile@gmail.com>
2022-08-04 16:08:47 +0300

    toDo

--- cypress/e2e/toDo.cy.js
+++ cypress/e2e/toDo.cy.js
@@ -17,9 +17,23 @@ cy.intercept('GET', 'https://api.realworld.io/api/articles/**').as('getArticles'
 
 cy.wait('@getArticles')
 
+const articleTags = {}
 
+        cy.get('.article-preview').eq(1).find('.tag-list li').then( articleTags => {
 
- 
+            cy.wrap(articleTags).each( tag => {
+
+               cy.get(tag).invoke('text').then( tagText => {
+
+                articleTags.test = tagText
+
+               })
+
+            })
+
+        })
+
+ cy.log(articleTags)
 
 })
 
